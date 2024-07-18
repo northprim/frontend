0679e92e49311df9e3a29af1af16fcf8289c0144
Miloš Maran <milos@northprim.com>
2024-07-18 09:25:35 +0200

    feat: move index.html, add vite config.server, add top level wc scripts

--- packages/web-components/vite.config.ts
+++ packages/web-components/vite.config.ts
@@ -2,6 +2,11 @@ import {defineConfig} from 'vite';
 import {resolve} from 'path';
 
 export default defineConfig({
+    server: {
+        open: true,
+        port: 3000,
+        strictPort: true
+    },
     build: {
         lib: {
             entry: [resolve(__dirname, 'lib/index.ts')],
